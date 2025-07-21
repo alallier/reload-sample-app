54fe91a46d4397273e32e4c3bc31b53cbbc29a45
Alexander J. Lallier <alexanderlallier@gmail.com>
2025-07-21 10:18:20 -0400

    Remove body parser dep, one because it is deprecated and secondly it isn't even needed in this example

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express')
 var http = require('http')
 var path = require('path')
 var reload = require('reload')
-var bodyParser = require('body-parser')
 var logger = require('morgan')
 
 var app = express()
@@ -11,7 +10,6 @@ var publicDir = path.join(__dirname, 'public')
 
 app.set('port', process.env.PORT || 3000)
 app.use(logger('dev'))
-app.use(bodyParser.json()) // Parses json, multi-part (file), url-encoded
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(publicDir, 'index.html'))
